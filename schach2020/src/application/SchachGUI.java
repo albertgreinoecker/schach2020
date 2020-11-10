3552040c8ed950ed4b17820d667967b7a8d04adc
albertgreinoecker <albert.greinoecker@gmail.com>
2020-11-10 19:34:37 +0100

    highlight possible moves

--- schach2020/src/application/SchachGUI.java
+++ schach2020/src/application/SchachGUI.java
@@ -16,15 +16,14 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-public class SchachGUI extends Application implements EventHandler <ActionEvent> { 
+public class SchachGUI extends Application implements EventHandler<ActionEvent> {
 	private SpielFeld sf = null;
 	private boolean first = true;
 	private Button[][] buttons = new Button[8][8];
 	private Label label = new Label();
-	private void imagesAufsFeld()
-	{
-		for (int i = 0; i < 8; i++)
-		{
+
+	private void imagesAufsFeld() {
+		for (int i = 0; i < 8; i++) {
 			for (int j = 0; j < 8; j++) {
 				String imgName = "img/" + sf.getFeld(j, i) + ".png";
 				Image im = new Image(imgName);
@@ -34,57 +33,68 @@ public class SchachGUI extends Application implements EventHandler <ActionEvent>
 			}
 		}
 	}
+
 	public void start(Stage primaryStage) throws FileNotFoundException {
 		sf = SpielFeldIO.einlesen("start.txt");
 		sf.ausgabe();
 		GridPane sfGrid = new GridPane();
 		BorderPane root = new BorderPane();
-		//Erzeuge leere Buttons
-		for (int i = 7; i >= 0; i--) //Zeile
+		// Erzeuge leere Buttons
+		for (int i = 7; i >= 0; i--) // Zeile
 		{
-			for (int j = 0; j  < 8; j++) { //Spalte
+			for (int j = 0; j < 8; j++) { // Spalte
 				Button b = new Button();
-				b.setPrefSize(1000, 1000); //so wachsen die Buttons mit wenn man vergrössert
+				b.setPrefSize(1000, 1000); // so wachsen die Buttons mit wenn man vergrössert
 				b.getStyleClass().add("field");
-				String id = ((char)(j + 'a')  + "" + (char)('8' - i));
+				String id = ((char) (j + 'a') + "" + (char) ('8' - i));
 				b.setId(id);
 				b.setOnAction(this);
 				b.setTooltip(new Tooltip(id));
-				if ((i - j) % 2 != 0)
-				{
+				if ((i - j) % 2 != 0) {
 					b.setStyle("-fx-background-color: #4a2700");
-				} else
-				{
+				} else {
 					b.setStyle("-fx-background-color: #FFFFFF");
 				}
 				buttons[j][i] = b;
-				sfGrid.add(b,j,i); //Achtung: (Zeile, Spalte)
+				sfGrid.add(b, j, i); // Achtung: (Zeile, Spalte)
 			}
 		}
 		imagesAufsFeld();
 		root.setCenter(sfGrid);
 		root.setBottom(label);
 		Scene scene = new Scene(root, 800, 800);
-		
+
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		primaryStage.setScene(scene);
 		primaryStage.show();
-	
-		
+
 	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
+
+	private void markiereSpielZugMoeglich(String von) {
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				String nach = buttons[i][j].getId();
+				String fullMove = von + "-" + nach;
+				if (sf.spielzugMoeglich(fullMove)) {
+					buttons[i][j].getStyleClass().add("field_selected");
+				}
+			}
+		}
+	}
+
 	@Override
 	public void handle(ActionEvent event) {
-		Node b = (Node)event.getSource();
-		if (first)
-		{
+		Node b = (Node) event.getSource();
+		if (first) {
 			b.getStyleClass().add("field_selected");
 			label.setText(b.getId());
-		} else
-		{
-			String  fullMove = label.getText() + "-" + b.getId();
+			markiereSpielZugMoeglich(b.getId());
+		} else {
+			String fullMove = label.getText() + "-" + b.getId();
 			System.out.println(fullMove);
 			sf.spielzug(fullMove);
 			imagesAufsFeld();
