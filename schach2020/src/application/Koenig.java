6192b89b9559a13f6bc6ba9100a364bdec6b35bf
albertgreinoecker <albert.greinoecker@gmail.com>
2020-11-10 19:36:03 +0100

    return superclass call of spielZugMoeglich

--- schach2020/src/application/Koenig.java
+++ schach2020/src/application/Koenig.java
@@ -7,6 +7,8 @@ public class Koenig extends Figur {
 
 	public boolean spielZugMoeglich(SpielFeld sp, Position von, Position nach) {
 		//TODO: rochade
+		if (!super.spielZugMoeglich(sp, von, nach)) return false;
+		
 		int dx = Math.abs(von.getX() - nach.getX());
 		int dy = Math.abs(von.getY() - nach.getY());
 		return dx <= 1 && dy <= 1;
