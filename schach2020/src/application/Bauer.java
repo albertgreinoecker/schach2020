c23f21c5a10d114ca93e1499126b40aaf184ef8c
albertgreinoecker <albert.greinoecker@gmail.com>
2020-11-10 19:35:33 +0100

    also check if there are other figures placed correctly

--- schach2020/src/application/Bauer.java
+++ schach2020/src/application/Bauer.java
@@ -6,21 +6,22 @@ public class Bauer extends Figur {
 	}
 
 	public boolean spielZugMoeglich(SpielFeld sp, Position von, Position nach) {
+		if (!super.spielZugMoeglich(sp, von, nach)) return false;
 		int dy = Math.abs(von.getY() - nach.getY());
 		int diffX = von.getX() - nach.getX();
-		if (dy == 0) // gerade fahren
+		if (dy == 0  && !sp.isFigur(nach)) // gerade fahren
 		{
 			if (farbeweiss) {
-				return (diffX == -1) || (!bewegt && diffX == -2);
+				return (diffX == 1) || (!bewegt && diffX == 2);
 
 			} else {
-				return (diffX == 1) || (!bewegt && diffX == 2);
+				return (diffX == -1) || (!bewegt && diffX == -2);
 			}
-		} else if (dy == 1) // schlagen
+		} else if (dy == 1 && sp.isFigur(nach)) // schlagen
 		{
 			// checken ob entsprechend der Farbe sich in die y-Position um genau eins
 			// unterscheidet
-			return (farbeweiss && diffX == -1) || (!farbeweiss && diffX == 1);
+			return (farbeweiss && diffX == 1) || (!farbeweiss && diffX == -1);
 		} else // alles andere ist ungueltig
 		{
 			return false;
