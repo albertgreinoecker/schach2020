f71aaa78276daf21e79a6ce801a4692fe5caf82d
albertgreinoecker <albert.greinoecker@gmail.com>
2020-11-11 10:15:46 +0100

    run all tests for whole test package

---
+++ schach2020/src/test_data/AllTest.java
@@ -0,0 +1,11 @@
+package test_data;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages("application.test")
+public class AllTest {
+
+}
